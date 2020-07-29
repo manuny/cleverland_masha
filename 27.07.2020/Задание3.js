97352fda44deefa1733f9da13907468fdd35a82a
unknown <vasilevamasha1982@gmail.com>
2020-07-30 00:43:34 +0300

    Задание3

---
+++ 27.07.2020/Задание3.js
@@ -0,0 +1,6 @@
+"use striсt";
+let color = document.querySelectorAll('ul > li');
+color[0].style.backgroundColor = 'blue';
+color[1].style.backgroundColor = 'green';
+color[2].style.backgroundColor = 'yellow';
+color[3].style.backgroundColor = 'orange'; 
